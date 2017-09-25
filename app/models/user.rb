101532995713a5961e856f193c58b9a91843e5e7
arup rakshit <aruprakshit@rocketmail.com>
2017-09-25 22:42:15 +0530

    Add sorcery to sign up/sign in and log out

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 4 }
+  validates :password, confirmation: true
+  validates :email, uniqueness: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  resources :users, only: %i[new create index]
+  get '/sign_up', to: 'users#new', as: :sign_up
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/log_in', to: 'sessions#new', as: :log_in
+  delete '/log_out', to: 'sessions#destroy', as: :log_out
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/oauth/callback', to: 'data#create_session'
   get '/index', to: 'data#index'
-  root to: 'data#index'
+  root to: 'users#index'
 end
