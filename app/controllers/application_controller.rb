61afbddd229c5cbf1cac8650ebb54e9e34db2036
arup rakshit <aruprakshit@rocketmail.com>
2017-09-27 17:00:40 +0530

    fix the controller hooks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def check_shutdown
+    logout if current_user && current_user.shutdown?
+  end
 end
--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,8 +5,4 @@ class AuthenticationsController < ApplicationController
     flash[:warning] = 'You have to authenticate to access this page.'
     redirect_to log_in_path
   end
-
-  def check_shutdown
-    logout if current_user && current_user.shutdown?
-  end
 end
